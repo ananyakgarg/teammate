c6f46e791bca7879572b2a644614baced72ab7b1
Meliora Ho <85920674+mellieho9@users.noreply.github.com>
2022-11-05 11:35:41 -0400

    created views folder

---
+++ teammate/Views/sign-in-view.swift
@@ -0,0 +1,20 @@
+//
+//  sign-in-view.swift
+//  teammate
+//
+//  Created by Clementine on 11/5/22.
+//
+
+import SwiftUI
+
+struct sign_in_view: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct sign_in_view_Previews: PreviewProvider {
+    static var previews: some View {
+        sign_in_view()
+    }
+}
---
+++ teammate/Views/signup.swift
@@ -0,0 +1,20 @@
+//
+//  signup.swift
+//  teammate
+//
+//  Created by Clementine on 11/5/22.
+//
+
+import SwiftUI
+
+struct signup: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct signup_Previews: PreviewProvider {
+    static var previews: some View {
+        signup()
+    }
+}
